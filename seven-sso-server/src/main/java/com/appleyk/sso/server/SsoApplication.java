255cc81f03529b4e332e81685dcec79652c11d08
appleyk <appleyk211>
2022-03-26 16:17:34 +0800

    commit sso-server application

---
+++ seven-sso-server/src/main/java/com/appleyk/sso/server/SsoApplication.java
@@ -0,0 +1,13 @@
+package com.appleyk.sso.server;
+
+/**
+ * <p></p>
+ *
+ * @author appleyk
+ * @version V.0.1.1
+ * @blob https://blog.csdn.net/appleyk
+ * @github https://github.com/kobeyk
+ * @date created on 2022/3/26-16:16
+ */
+public class SsoApplication {
+}
