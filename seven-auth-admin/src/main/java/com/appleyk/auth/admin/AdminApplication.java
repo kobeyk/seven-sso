7f3a53935e8c7acf9c71fce0ad15845900726106
appleyk <yukun24@126.com>
2022-05-03 15:45:18 +0800

    增加admin模块（用户认证控制台）和ui（前后端分离react前端站点）模块

---
+++ seven-auth-admin/src/main/java/com/appleyk/auth/admin/AdminApplication.java
@@ -0,0 +1,20 @@
+package com.appleyk.auth.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>越努力，越幸运</p>
+ *
+ * @author appleyk
+ * @version V.0.1.1
+ * @blob https://blog.csdn.net/appleyk
+ * @github https://github.com/kobeyk
+ * @date created on  下午3:25 2022/5/3
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class,args);
+    }
+}
---
+++ seven-sso-ui/README.md
@@ -0,0 +1,4 @@
+1、mkdir seven-sso-ui && cd seven-sso-ui
+2、yarn create @umijs/umi-app
+3、yarn
+4、yarn start
